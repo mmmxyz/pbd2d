c7165207e918072d1097a233db137f4e33536a45
pb <81746011+poly-bear@users.noreply.github.com>
2021-06-19 17:41:56 +0900

    bend

--- project/doublerec/main.cpp
+++ project/doublerec/main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <memory>
 
+#include <unistd.h>
+
 #include "mathfunc/vec.hpp"
 #include "mathfunc/matrix.hpp"
 
@@ -16,7 +18,7 @@
 constexpr uint32_t FPS = 120;
 constexpr float dt = 1.0 / FPS;
 
-constexpr float epsilon = 0.0001;
+constexpr float epsilon = 0.001;
 
 class constraint
 {
@@ -41,7 +43,7 @@ class collision : public constraint
 		void projection(float sn) override
 		{
 				float C = (p - q).dot(normal);
-				if (C > 0.0) // not collide
+				if (C > epsilon) // not collide
 						return;
 
 				fvec2 dC = normal;
@@ -69,6 +71,9 @@ class distance : public constraint
 		void projection(float sn) override
 		{
 				float l = (p0 - p1).length();
+				//if (std::abs(l - L) < epsilon)
+				//		return;
+
 				fvec2 dposi0 = (p1 - p0).normalize() * ((w0) / (w0 + w1)) * (l - L);
 				fvec2 dposi1 = (p0 - p1).normalize() * ((w1) / (w0 + w1)) * (l - L);
 
@@ -79,38 +84,101 @@ class distance : public constraint
 
 class bend : public constraint
 {
-		const float m0, m1;
-		const float w0, w1;
+		const float m0, m1, m2;
+		const float w0, w1, w2;
 		const float theta;
 		fvec2 &p0;
 		fvec2 &p1;
-		fvec2 &ref;
+		fvec2 &p2;
 
 	  public:
-		bend(float m0, float m1, float theta, fvec2 &p0, fvec2 &p1, fvec2 &ref)
-			: m0(m0), m1(m1), w0(1.0 / m0), w1(1.0 / m1), theta(theta), p0(p0), p1(p1), ref(ref)
+		bend(float m0, float m1, float m2, float theta, fvec2 &p0, fvec2 &p1, fvec2 &p2)
+			: m0(m0), m1(m1), m2(m2), w0(1.0 / m0), w1(1.0 / m1), w2(1.0 / m2), theta(theta), p0(p0), p1(p1), p2(p2)
 		{
 		}
 
 		void projection(float sn) override
 		{
-				fvec2 v0 = (p0 - ref).normalize();
-				fvec2 v1 = (p1 - ref).normalize();
+				fvec2 v0 = (p0 - p1).normalize();
+				fvec2 v2 = (p2 - p1).normalize();
 
-				float v0dv1 = v0.dot(v1);
-				float C = std::acos(v0dv1) - theta;
+				float v0dv2 = v0.dot(v2);
+				float C = std::acos(v0dv2) - theta;
 				if (std::isnan(C))
 						return;
-				float darc = -1.0 / (std::sqrt(1 - v0dv1 * v0dv1));
 
-				fvec2 dC0 = darc * (1.0 / (p0 - ref).length()) * (v1 - v0dv1 * v0);
-				fvec2 dC1 = darc * (1.0 / (p1 - ref).length()) * (v0 - v0dv1 * v1);
+				//std::cout << "hoge" << std::endl;
 
-				fvec2 dp0 = -1.0 * C / (w0 * dC0.sqlength() + w1 * dC1.sqlength()) * w0 * dC0;
-				fvec2 dp1 = -1.0 * C / (w0 * dC0.sqlength() + w1 * dC1.sqlength()) * w1 * dC1;
+				//std::cout << std::acos(v0dv2) << std::endl;
+				//std::cout << v0dv2 << std::endl;
 
-				p0 = p0 + dp0;
-				p1 = p1 + dp1;
+				float darc = -1.0 / (std::sqrt(1 - v0dv2 * v0dv2));
+				//std::cout << darc << std::endl;
+				if (std::isnan(darc) || std::isinf(darc))
+						return;
+
+				fvec2 dC0 = (1.0 / (p0 - p1).length()) * (v2 - v0dv2 * v0);
+				fvec2 dC2 = (1.0 / (p2 - p1).length()) * (v0 - v0dv2 * v2);
+				fvec2 dC1 = -dC0 - dC2;
+
+				//std::cout << dC0 << std::endl;
+				//std::cout << dC2 << std::endl;
+				//std::cout << dC1 << std::endl;
+
+				dC0 = darc * dC0;
+				dC2 = darc * dC2;
+				dC1 = darc * dC1;
+
+				//std::cout << dC0 << std::endl;
+				//std::cout << dC2 << std::endl;
+				//std::cout << dC1 << std::endl;
+
+				fvec2 dp0 = -1.0 * C / (w0 * dC0.sqlength() + w2 * dC2.sqlength() + w1 * dC1.sqlength()) * w0 * dC0;
+				fvec2 dp2 = -1.0 * C / (w0 * dC0.sqlength() + w2 * dC2.sqlength() + w1 * dC1.sqlength()) * w2 * dC2;
+				fvec2 dp1 = -1.0 * C / (w0 * dC0.sqlength() + w2 * dC2.sqlength() + w1 * dC1.sqlength()) * w1 * dC1;
+
+				//std::cout << dp0 << std::endl;
+
+				p0 = p0 + sn * dp0;
+				p2 = p2 + sn * dp2;
+				p1 = p1 + sn * dp1;
+		}
+};
+
+class bendsign : public constraint
+{
+		const float m0, m1, m2;
+		const float w0, w1, w2;
+		fvec2 &p0;
+		fvec2 &p1;
+		fvec2 &p2;
+
+	  public:
+		bendsign(float m0, float m1, float m2, fvec2 &p0, fvec2 &p1, fvec2 &p2)
+			: m0(m0), m1(m1), m2(m2), w0(1.0 / m0), w1(1.0 / m1), w2(1.0 / m2), p0(p0), p1(p1), p2(p2)
+		{
+		}
+
+		void projection(float sn) override
+		{
+				fvec2 v0 = (p0 - p1).normalize();
+				fvec2 v2 = (p2 - p1).normalize();
+
+				float C = v2.cross(v0);
+				if (C > 0.0)
+						return;
+
+				fvec2 dC0(-v2.y, v2.x);
+				fvec2 dC1(-v0.y + v2.y, v0.x - v2.x);
+				fvec2 dC2(v0.y, -v0.x);
+
+				fvec2 dp0 = -1.0 * C / (w0 * dC0.sqlength() + w2 * dC2.sqlength() + w1 * dC1.sqlength()) * w0 * dC0;
+				fvec2 dp2 = -1.0 * C / (w0 * dC0.sqlength() + w2 * dC2.sqlength() + w1 * dC1.sqlength()) * w2 * dC2;
+				fvec2 dp1 = -1.0 * C / (w0 * dC0.sqlength() + w2 * dC2.sqlength() + w1 * dC1.sqlength()) * w1 * dC1;
+
+				p0 = p0 + sn * dp0;
+				p2 = p2 + sn * dp2;
+				p1 = p1 + sn * dp1;
 		}
 };
 
@@ -213,64 +281,106 @@ class rect : public object
 				}
 		}
 
-		bool is_collidebar(const fvec2 a0, const fvec2 a1, const fvec2 b0, const fvec2 b1) const
+		bool is_collidebarline(const fvec2 a0, const fvec2 a1, const fvec2 b0, const fvec2 b1) const
 		{
+				//line a0,a1
+				//bar b0,b1
 				fvec2 v = b0 - a0;
 				fvec2 w = b1 - a0;
 
-				return v.cross(a1 - a0) * w.cross(a1 - a0) < -0.0;
+				return v.cross(a1 - a0) * w.cross(a1 - a0) < 0.0;
+		}
+
+		bool is_collidebarbar(const fvec2 a0, const fvec2 a1, const fvec2 b0, const fvec2 b1) const
+		{
+
+				return is_collidebarline(a0, a1, b0, b1) && is_collidebarline(b0, b1, a0, a1);
+		}
+
+		bool is_collidebarray(const fvec2 a0, const fvec2 a1, const fvec2 b0, const fvec2 b1) const
+		{
+				//ray a0 to a1
+				//bar b0,b1
+
+				if (is_collidebarline(a0, a1, b0, b1))
+				{
+						fmat2 hoge(a1 - a0, b0 - b1);
+						hoge = hoge.inverse();
+						fvec2 ts = hoge * (b0 - a0);
+						if (ts.x >= 0.0)
+								return true;
+				}
+				return false;
+		}
+
+		bool is_inside(const fvec2 &v)
+		{
+
+				fvec2 center = (p[0] + p[1] + p[2] + p[3]) / 4.0;
+
+				fvec2 v0 = v - p[0];
+				fvec2 x = p[1] - p[0];
+				fvec2 y = p[3] - p[0];
+				float xc = x.dot(v0);
+				float yc = y.dot(v0);
+
+				if (epsilon < xc && xc < width * width - epsilon && epsilon < yc && yc < height * height - epsilon)
+						return true;
+				else
+						return false;
 		}
 
 		void createcollide(rect &r, std::vector<std::shared_ptr<constraint>> &consset)
 		{
 
+				float ratio = (m[0] + m[1] + m[2] + m[3]) / (r.m[0] + r.m[1] + r.m[2] + r.m[3]);
+
+				fvec2 rgv = (r.v[0] + r.v[1] + r.v[2] + r.v[3]) / 4;
+
 				for (uint32_t i = 0; i < 4; i++)
 				{
-						uint32_t ii = (i + 1) % 4;
-						for (uint32_t j = 0; j < 4; j++)
+						if (r.is_inside(p[i]))
 						{
-								uint32_t jj = (j + 1) % 4;
 
-								//2つのrectについて、4*4=16通りの辺の組み合わせで衝突判定
-								if (is_collidebar(p[i], p[ii], r.p[j], r.p[jj]) &&
-									is_collidebar(r.p[jj], r.p[j], p[i], p[ii])
+								float ccolor[4] = {0.0, 1.0, 1.0, 0.0};
+								float cposi[2] = {p[i].x - 0.001f, p[i].y + 0.001f};
+								point1d contact(cposi, ccolor);
+								contact.draw();
 
-								)
+								float wcolor[4] = {1.0, 1.0, 0.0, 0.0};
+								float hogep[2] = {p[i].x, p[i].y};
+								float hogepv[2] = {p[i].x - (v[i].x - rgv.x), p[i].y - (v[i].y - rgv.y)};
+								line2d contactlod(hogep, hogepv, wcolor);
+								contactlod.draw();
+
+								for (uint32_t j = 0; j < 4; j++)
 								{
-										fmat2 hoge(p[ii] - p[i], r.p[j] - r.p[jj]);
-										hoge = hoge.inverse();
-										fvec2 ts = hoge * (r.p[j] - p[i]);
-										//衝突ポイント collision point
-										fvec2 cp = ts.x * (p[ii] - p[i]) + p[i];
+										uint32_t jj = (j + 1) % 4;
 
-										//test cpを表示 debug用
-										float ccolor[4] = {0.0, 0.0, 0.0, 0.0};
-										float cposi[2] = {cp.x, cp.y};
-										point1d contact(cposi, ccolor);
-										contact.draw();
-										float wcolor[4] = {0.0, 0.0, 1.0, 0.0};
-										//
-
-										//貫通している法の頂点を押し出す。
-										if ((p[ii] - p[i]).cross(r.p[j] - p[i]) < 0.0)
+										if (is_collidebarray(p[i], p[i] - (v[i] - rgv), r.p[j], r.p[jj]))
 										{
+
+												fvec2 normal((r.p[jj] - r.p[j]).y, -(r.p[jj] - r.p[j]).x);
+												normal = normal.normalize();
+
+												fmat2 hoge(normal, r.p[j] - r.p[jj]);
+												hoge = hoge.inverse();
+												fvec2 ts = hoge * (r.p[j] - p[i]);
+												////衝突ポイント collision point
+												fvec2 cp = ts.x * normal + p[i];
+
+												//test
+												float ccolor[4] = {0.0, 0.0, 0.0, 0.0};
+												float cposi[2] = {cp.x, cp.y};
+												point1d contact(cposi, ccolor);
+												contact.draw();
+
 												consset.emplace_back(
-													std::make_shared<collision>(p[i], cnormal[i], cp, 0.5, cp - p[i]));
-
-												//debug用
-												float hogep[2] = {p[i].x, p[i].y};
-												line2d contactlod(hogep, cposi, wcolor);
-												contactlod.draw();
-										}
-										else
-										{
-												consset.emplace_back(std::make_shared<collision>(p[ii], cnormal[ii], cp,
-																								 0.5, cp - p[ii]));
-
-												//debug用
-												float hogep[2] = {p[ii].x, p[ii].y};
-												line2d contactlod(hogep, cposi, wcolor);
-												contactlod.draw();
+													std::make_shared<collision>(p[i], cnormal[i], cp, ratio, normal));
+												consset.emplace_back(std::make_shared<collision>(
+													r.p[j], r.cnormal[j], cp, 0.5 / ratio, -normal));
+												consset.emplace_back(std::make_shared<collision>(
+													r.p[jj], r.cnormal[jj], cp, 0.5 / ratio, -normal));
 										}
 								}
 						}
@@ -289,6 +399,7 @@ class rect : public object
 		{
 				for (uint32_t i = 0; i < 4; i++)
 				{
+
 						v[i] = (p[i] - x[i]) / dt;
 						x[i] = p[i];
 
@@ -323,8 +434,12 @@ class rect : public object
 						consset.emplace_back(std::make_shared<distance>(m[i], m[j], p[i], p[j], L));
 
 						uint32_t jj = (i + 2) % 4;
-						consset.emplace_back(std::make_shared<bend>(m[i], m[jj], 3.14 * 0.5, p[i], p[jj], p[j]));
+						consset.emplace_back(std::make_shared<bend>(m[i], m[j], m[jj], 3.14 * 0.5, p[i], p[j], p[jj]));
+						consset.emplace_back(std::make_shared<bendsign>(m[i], m[j], m[jj], p[i], p[j], p[jj]));
 				}
+				//float L = std::sqrt(width * width + height * height);
+				//consset.emplace_back(std::make_shared<distance>(m[0], m[2], p[0], p[2], L));
+				//consset.emplace_back(std::make_shared<distance>(m[1], m[3], p[1], p[3], L));
 		}
 
 		void createwallcollision(std::vector<std::shared_ptr<constraint>> &consset) override
@@ -363,14 +478,14 @@ void timestep(rect &r0, rect &r1)
 		float sn = 1.0;
 
 		//solver
-		for (uint32_t loopcounter = 0; loopcounter < 30; loopcounter++)
+		for (uint32_t loopcounter = 0; loopcounter < 10; loopcounter++)
 		{
 				for (auto &x : icv)
 						x->projection(sn);
 
 				for (auto &x : ecv)
 						x->projection(sn);
-				sn *= 1.0;
+				sn *= 0.9;
 		}
 
 		r0.refineposition();
@@ -400,10 +515,10 @@ int main(int argc, char const *argv[])
 		line2d wl3(wp3, wp0, wcolor);
 
 		float color0[4] = {1.0, 0.0, 0.0, 0.0};
-		rect r0(1.0, fvec2(0.0, 0.5), 0.3, 0.2, fvec2(0.0, -3.0), -0.05, color0);
+		rect r0(2.0, fvec2(-0.4, 0.5), 0.3, 0.4, fvec2(2.0, 5.0), -50.5, color0);
 
 		float color1[4] = {0.0, 1.0, 0.0, 0.0};
-		rect r1(1.0, fvec2(0.1, -0.7), 0.4, 0.1, fvec2(0.0, 0.5), 0.08, color1);
+		rect r1(1.0, fvec2(0.4, -0.6), 0.4, 0.1, fvec2(-2.0, 4.5), 50.8, color1);
 
 		double ctime = 0.0;
 		double ltime = 0.0;
@@ -437,7 +552,6 @@ int main(int argc, char const *argv[])
 				//wait event
 				mywindow.clearstatus();
 				mywindow.pollevent();
-				//mywindow.waitevent(100);
 
 				step++;
 				cout << "step: " << step;
@@ -449,6 +563,11 @@ int main(int argc, char const *argv[])
 				//		mywindow.waitevent(100);
 				//		std::cout << "waiting...." << std::endl;
 				//}
+
+				//for debug
+				//mywindow.waitevent(100);
+				//usleep(10000);
+				//mywindow.waitevent(100);
 		}
 
 		return 0;
